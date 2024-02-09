417cd5c0b7a28cebede85d986540e5169a5779a7
Parasuraman8 <parasuraman8cs@gmail.com>
2024-02-09 12:52:56 +0530

    -

--- Python/gcd.py
+++ Python/gcd.py
@@ -1,13 +1,7 @@
-a,b = int(input("enter the a :")),int(input("enter the b  :"))
+def findGCD(a,b):
+    while(b != 0):
+        a , b = b, a % b
+    return a 
 
-if a<b:
-    smaller = a
-else:
-    smaller = b
-
-for i in range(1,smaller+1):
-    if( (a%i == 0) and (b%i == 0) ):
-        hcf = i
-        print(hcf)
-
-print("The H.C.F of ",a ," and ", b  ," is ", hcf)
+n1,n2 = int(input("Enter the N1: ")),int(input("Enter the N2: "))
+print("The GCD of {} and {} is {}".format(n1,n2,findGCD(n1,n2)))
---
+++ Python/hcf.py
@@ -0,0 +1,13 @@
+a,b = int(input("enter the a :")),int(input("enter the b  :"))
+
+if a<b:
+    smaller = a
+else:
+    smaller = b
+
+for i in range(1,smaller+1):
+    if( (a%i == 0) and (b%i == 0) ):
+        hcf = i
+        print(hcf)
+
+print("The HCF of ",a ," and ", b  ," is ", hcf)
